4c6ec3d48508eced39c2f9b0e246ae8eb327cf09
peter <peteradetunji30@gmail.com>
2024-02-09 19:46:56 +0000

    automating chrome browser

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright, Playwright
+import time
 
 def run(playwright:Playwright):
     title_list = []
@@ -10,5 +11,27 @@ def run(playwright:Playwright):
     print(f'we have {len(title_list)} data')
     # print(title_list)
 
+def main(playwright: Playwright):
+    chrome = playwright.chromium
+    print('launchinh a browser')
+    browser = chrome.launch(headless=False)
+    print('launching a new page')
+    page = browser.new_page()
+    print('visiting a new url')
+    page.goto('https://www.google.com/webhp')
+    word = 'Hello world'
+    page.get_by_title('Search').fill(word)
+    page.keyboard.press('Enter')
+    # page.locator('div').get_by_text('Images').click()
+    page.get_by_role('link', name="Images", exact=True).click()
+    time.sleep(5)
+    new = word.split( )
+    print(new[0])
+    page.get_by_role('button', name=new[0]).first.click()
+    time.sleep(10)
+    print('text successfully sed')
+    browser.close()
+    print('browser closed successfully')
+
 with sync_playwright() as playwright:
-    run(playwright)
+    main(playwright)
